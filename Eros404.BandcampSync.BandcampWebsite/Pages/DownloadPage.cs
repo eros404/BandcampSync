1606a5d621f1c048bf0c3c98eefd0efdc0b93af8
Louis Morando <louis.morando@epita.fr>
2022-11-15 12:53:49 +0100

    oui

---
+++ Eros404.BandcampSync.BandcampWebsite/Pages/DownloadPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace Eros404.BandcampSync.BandcampWebsite.Pages
+{
+    internal class DownloadPage : Page<DownloadPage>
+    {
+        public DownloadPage(IWebDriver driver, string baseUrl) : base(driver, baseUrl)
+        {
+        }
+
+        protected override void ExecuteLoad()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool EvaluateLoadedStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
